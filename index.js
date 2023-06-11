67aea52b955a8022e79d2becdedb36b64a04ff10
ALIMUZZAMAN <alimuzzamanzamaddar@gmail.com>
2023-06-11 16:28:20 +0600

    corse is done

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const classCollection = client.db("sportsDB").collection("class");
     const paidCollection = client.db("sportsDB").collection("paid");
