6aa711072a73df57b311958aee862619b49129a1
ALIMUZZAMAN <alimuzzamanzamaddar@gmail.com>
2023-06-12 02:19:25 +0600

    complete

--- index.js
+++ index.js
@@ -34,8 +34,6 @@ const verifyJWT = (req, res, next) => {
 };
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wjtuflr.mongodb.net/?retryWrites=true&w=majority`;
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fmznhrh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
